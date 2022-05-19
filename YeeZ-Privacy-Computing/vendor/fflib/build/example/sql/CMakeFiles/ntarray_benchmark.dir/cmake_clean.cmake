82cfca3ab28586c78e85d10616c01e7fdd4524ab
SishiCheng <70729850+SishiCheng@users.noreply.github.com>
2022-05-18 19:07:05 -0600

    Add files via upload

---
+++ YeeZ-Privacy-Computing/vendor/fflib/build/example/sql/CMakeFiles/ntarray_benchmark.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../../bin/ntarray_benchmark"
+  "../../../bin/ntarray_benchmark.pdb"
+  "CMakeFiles/ntarray_benchmark.dir/ntarray_benchmark.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ntarray_benchmark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ YeeZ-Privacy-Computing/vendor/fflib/build/example/sql/CMakeFiles/table.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../../bin/table"
+  "../../../bin/table.pdb"
+  "CMakeFiles/table.dir/use_table.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/table.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
