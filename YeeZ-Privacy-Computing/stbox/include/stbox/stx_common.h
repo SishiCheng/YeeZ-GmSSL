d7ee7c895dc93ad1c22fa29c29c19ad4b7fe4907
SishiCheng <70729850+SishiCheng@users.noreply.github.com>
2022-05-18 18:47:47 -0600

    Delete YeeZ-Privacy-Computing/stbox/include/stbox directory

--- YeeZ-Privacy-Computing/stbox/include/stbox/stx_common.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <cstddef>
-#include <cstdint>
-
-namespace stbox {
-int printf(const char *fmt, ...);
-int sprintf(char *buf, const char *fmt, ...);
-void print_hex(uint8_t *data, size_t data_len);
-} // namespace stbox
--- YeeZ-Privacy-Computing/stbox/include/stbox/usgx/error_message.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <sgx_edger8r.h>
-#include <string>
-
-namespace std {
-std::string to_string(sgx_status_t ret);
-}
