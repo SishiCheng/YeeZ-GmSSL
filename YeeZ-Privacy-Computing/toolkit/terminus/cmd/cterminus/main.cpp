1bec774db21950d7e09a4f98e96e78297d949d96
SishiCheng <70729850+SishiCheng@users.noreply.github.com>
2022-05-18 18:36:21 -0600

    Delete YeeZ-Privacy-Computing/toolkit directory

--- YeeZ-Privacy-Computing/toolkit/terminus/cmd/cterminus/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "cmd_line.h"
-
-int main(int argc, char *argv[]) {
-  auto tp = parse_command_line(argc, argv);
-  auto crypto = ypc::terminus::intel_sgx_and_eth_compatible();
-  return std::get<1>(tp)(crypto.get());
-}
