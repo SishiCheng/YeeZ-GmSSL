7a7dbdd7efb2e80fae90540104168362cd6fad0d
SishiCheng <70729850+SishiCheng@users.noreply.github.com>
2022-05-18 18:25:59 -0600

    Delete YeeZ-Privacy-Computing/core directory

--- YeeZ-Privacy-Computing/core/include/ypc_t/analyzer/internal/is_multi_datasource.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-namespace ypc {
-namespace internal {
-
-template <typename DataSession> struct is_multi_datasource {
-  static constexpr bool value = false;
-};
-} // namespace internal
-} // namespace ypc
--- YeeZ-Privacy-Computing/core/include/ypc_t/analyzer/internal/is_param_encrypted.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-namespace ypc {
-namespace internal {
-
-template <typename Result> struct is_param_encrypted {
-  static constexpr bool value = true;
-};
-} // namespace internal
-} // namespace ypc
