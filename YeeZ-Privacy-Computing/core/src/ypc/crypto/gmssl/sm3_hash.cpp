8bc888180605edfec327e3d633a54648a976bedc
SishiCheng <70729850+SishiCheng@users.noreply.github.com>
2022-05-24 00:04:30 -0600

    Update sm3_hash.cpp

--- YeeZ-Privacy-Computing/core/src/ypc/crypto/gmssl/sm3_hash.cpp
+++ YeeZ-Privacy-Computing/core/src/ypc/crypto/gmssl/sm3_hash.cpp
@@ -12,10 +12,13 @@ uint32_t sm3_hash::get_msg_hash_size() { return 32; }
 
 uint32_t sm3_hash::msg_hash(const uint8_t *raw_msg, uint32_t msg_size,
                            uint8_t *hash, uint32_t hash_size) {
+	SM3_CTX ctx;
+	sm3_init(&ctx);
+	sm3_update(&ctx, raw_msg, msg_size);
 	uint8_t dgst[32];
-	memcpy(dgst, hash, 32);
-	return sm3_digest(raw_msg, msg_size, dgst, 32);
+	sm3_finish(&ctx, dgst);
+	memcpy(hash, dgst, 32);
+	return stbox::stx_status::success;
 }
 } // namespace crypto
 } // namespace ypc
-
