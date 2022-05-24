aaff26f71397dc74d50762c9d75d7530393d06ce
SishiCheng <70729850+SishiCheng@users.noreply.github.com>
2022-05-23 20:21:53 -0600

    Add files via upload

---
+++ YeeZ-Privacy-Computing/core/include/corecommon/crypto/gmssl/sm2.h
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace ypc {
+namespace crypto {
+struct sm2 {
+  inline static uint32_t get_private_key_size() { return 32; }
+  inline static uint32_t get_public_key_size() { return 64; }
+  static uint32_t gen_private_key(uint32_t skey_size, uint8_t *skey);
+
+  static uint32_t generate_pkey_from_skey(const uint8_t *skey,
+                                          uint32_t skey_size, uint8_t *pkey,
+                                          uint32_t pkey_size);
+
+  static uint32_t get_signature_size();
+
+  static uint32_t sign_message(const uint8_t *skey, uint32_t skey_size,
+                               const uint8_t *data, uint32_t data_size,
+                               uint8_t *sig, uint32_t sig_size);
+
+  static uint32_t verify_signature(const uint8_t *data, uint32_t data_size,
+                                   const uint8_t *sig, uint32_t sig_size,
+                                   const uint8_t *public_key,
+                                   uint32_t pkey_size);
+};
+} // namespace crypto
+} // namespace ypc
