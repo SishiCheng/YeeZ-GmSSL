48e84ee01d7bd269b9be1b45cfddcd465cb20495
SishiCheng <70729850+SishiCheng@users.noreply.github.com>
2022-05-26 00:51:02 -0600

    Update sm2_ecc.cpp

--- YeeZ-Privacy-Computing/core/src/ypc/crypto/gmssl/sm2_ecc.cpp
+++ YeeZ-Privacy-Computing/core/src/ypc/crypto/gmssl/sm2_ecc.cpp
@@ -2,6 +2,7 @@
 #include <gmssl/sm2.h>
 #include "common/byte.h"
 #include "stbox/stx_status.h"
+using namespace std;
 extern "C" {
 #include "stbox/keccak/keccak.h"
 }
@@ -10,25 +11,34 @@ namespace crypto {
 
 uint32_t sm2_ecc::get_private_key_size() { return 32; }
 
-uint32_t sm2_ecc::get_public_key_size() { return 256; }
+uint32_t sm2_ecc::get_public_key_size() { return 64; }
 
 uint32_t sm2_ecc::gen_private_key(uint32_t skey_size, uint8_t *skey) {
-    struct SM2_KEY *p_key = nullptr;
+    SM2_KEY *p_key = nullptr;
     uint8_t tmp[skey_size];
     memcpy(tmp, skey, skey_size);
     sm2_key_set_private_key(p_key, tmp);
     return stbox::stx_status::success;
 }
 
+/* 
+ * Goal:   generate public key form private key
+ * Input:  skey - private key
+ *         skey_size 
+ *         pkey - public key
+ *         pkey_size
+ * Output: stx_status
+*/
 uint32_t sm2_ecc::generate_pkey_from_skey(const uint8_t *skey,
                                           uint32_t skey_size, uint8_t *pkey,
                                           uint32_t pkey_size) {
-    struct SM2_KEY tmp;
-    struct SM2_POINT tmp_pkey;
-    tmp_pkey.x = 
-    tmp.public_key = 
-    return ecc_t::generate_pkey_from_skey(skey, skey_size, pkey, pkey_size);
+    SM2_KEY *tmp; 
+    uint8_t skey_arr[32];
+    memcpy(skey_arr, skey, skey_size);
+    sm2_key_set_private_key(tmp, skey_arr);
+    return stbox::stx_status::success;
 }
+
+
 } // namespace crypto
 } // namespace ypc
-
