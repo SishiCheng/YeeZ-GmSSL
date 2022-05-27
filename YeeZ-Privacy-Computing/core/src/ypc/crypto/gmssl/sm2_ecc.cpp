5d5dbe8f01d7b6ee2963b01075b9bcb94bdb1982
SishiCheng <70729850+SishiCheng@users.noreply.github.com>
2022-05-26 21:37:28 -0600

    Update sm2_ecc.cpp

--- YeeZ-Privacy-Computing/core/src/ypc/crypto/gmssl/sm2_ecc.cpp
+++ YeeZ-Privacy-Computing/core/src/ypc/crypto/gmssl/sm2_ecc.cpp
@@ -15,7 +15,9 @@ uint32_t sm2_ecc::get_private_key_size() { return 32; }
 uint32_t sm2_ecc::get_public_key_size() { return 64; }
 
 uint32_t sm2_ecc::gen_private_key(uint32_t skey_size, uint8_t *skey) {
-    rand_bytes(skey, skey_size);
+    SM2_KEY *tmp = new SM2_KEY(); 
+    sm2_key_generate(tmp);
+    memcpy(skey, tmp->private_key, skey_size);
     return stbox::stx_status::success;
 }
 
@@ -30,10 +32,8 @@ uint32_t sm2_ecc::gen_private_key(uint32_t skey_size, uint8_t *skey) {
 uint32_t sm2_ecc::generate_pkey_from_skey(const uint8_t *skey,
                                           uint32_t skey_size, uint8_t *pkey,
                                           uint32_t pkey_size) {
-    SM2_KEY *tmp; 
-    uint8_t skey_arr[32];
-    memcpy(skey_arr, skey, skey_size);
-    sm2_key_set_private_key(tmp, skey_arr);
+    SM2_KEY *tmp = new SM2_KEY(); 
+    sm2_key_set_private_key(tmp, skey);
     memcpy(pkey, tmp->public_key.x, 32);
     memcpy(pkey + 32, tmp->public_key.y, 32);
     return stbox::stx_status::success;
