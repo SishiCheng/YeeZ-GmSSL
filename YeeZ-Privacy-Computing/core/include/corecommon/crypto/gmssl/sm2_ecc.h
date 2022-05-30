c3e10f704b505605b1dbfd4e7bcff323e945d9c4
SishiCheng <70729850+SishiCheng@users.noreply.github.com>
2022-05-29 22:21:56 -0400

    Update sm2_ecc.h

--- YeeZ-Privacy-Computing/core/include/corecommon/crypto/gmssl/sm2_ecc.h
+++ YeeZ-Privacy-Computing/core/include/corecommon/crypto/gmssl/sm2_ecc.h
@@ -1,18 +1,30 @@
 #pragma once
 #include <cstdint>
 
-namespace ypc{
-namespace crypto{
+namespace ypc {
+namespace crypto {
+struct sm2_ecc {
+  inline static uint32_t get_private_key_size();
+  inline static uint32_t get_public_key_size();
+  static uint32_t gen_private_key(uint32_t skey_size, uint8_t *skey);
 
-class sm2_ecc{
-public:
-    static uint32_t get_private_key_size();
-    static uint32_t get_public_key_size();
-     static uint32_t gen_private_key(uint32_t skey_size, uint8_t *skey);
-    static uint32_t generate_pkey_from_skey(const uint8_t *skey,
+  static uint32_t generate_pkey_from_skey(const uint8_t *skey,
                                           uint32_t skey_size, uint8_t *pkey,
                                           uint32_t pkey_size);
-};
 
-}
+  static uint32_t sign_message(const uint8_t *skey, uint32_t skey_size,
+                               const uint8_t *data, uint32_t data_size,
+                               uint8_t *sig, uint32_t sig_size);
+
+  static uint32_t verify_signature(const uint8_t *data, uint32_t data_size,
+                                   const uint8_t *sig, uint32_t sig_size,
+                                   const uint8_t *public_key,
+                                   uint32_t pkey_size);
+
+  static uint32_t ecdh_shared_key(const uint8_t *skey, uint32_t skey_size,
+                                  const uint8_t *public_key, uint32_t pkey_size,
+                                  uint8_t *shared_key,
+                                  uint32_t shared_key_size);
+};
+} // namespace crypto
 } // namespace ypc
