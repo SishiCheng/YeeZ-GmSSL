b0f78623cff6f8b765f78f42e1f6cde46724bbc6
SishiCheng <70729850+SishiCheng@users.noreply.github.com>
2022-05-30 00:14:29 -0400

    Update sm2_ecc.cpp

--- YeeZ-Privacy-Computing/core/src/ypc/crypto/gmssl/sm2_ecc.cpp
+++ YeeZ-Privacy-Computing/core/src/ypc/crypto/gmssl/sm2_ecc.cpp
@@ -1,4 +1,5 @@
 #include "corecommon/crypto/gmssl/sm2_ecc.h"
+#include "corecommon/crypto/gmssl/sm3_hash.h"
 #include <gmssl/sm2.h>
 #include "common/byte.h"
 #include "stbox/stx_status.h"
@@ -14,7 +15,12 @@ uint32_t sm2_ecc::get_public_key_size() { return 64; }
 
 uint32_t sm2_ecc::gen_private_key(uint32_t skey_size, uint8_t *skey) {
     SM2_KEY tmp; 
-    sm2_key_generate(&tmp);
+    int res = sm2_key_generate(&tmp);
+    if (res == -1)
+    {
+        return stbox::stx_status::sm2_empty_key;
+    }
+    
     memcpy(skey, tmp.private_key, skey_size);
     return stbox::stx_status::success;
 }
@@ -23,7 +29,12 @@ uint32_t sm2_ecc::generate_pkey_from_skey(const uint8_t *skey,
                                           uint32_t skey_size, uint8_t *pkey,
                                           uint32_t pkey_size) {
     SM2_KEY tmp; 
-    sm2_key_set_private_key(&tmp, skey);
+    int res = sm2_key_set_private_key(&tmp, skey);
+    if (res == -1)
+    {
+        return stbox::stx_status::sm2_point_generate_error;
+    }
+
     memcpy(pkey, tmp.public_key.x, 32);
     memcpy(pkey + 32, tmp.public_key.y, 32);
     return stbox::stx_status::success;
@@ -32,14 +43,22 @@ uint32_t sm2_ecc::generate_pkey_from_skey(const uint8_t *skey,
 uint32_t sm2_ecc::sign_message(const uint8_t *skey, uint32_t skey_size,
                              const uint8_t *data, uint32_t data_size,
                              uint8_t *sig, uint32_t sig_size){
-    uint8_t pkey;
-    sm2_ecc::generate_pkey_from_skey(skey, skey_size, &pkey, 64);
+    uint8_t hash[32];
+    sm3_hash::msg_hash(data, data_size, hash, 32);
     SM2_KEY key;
-    memcpy(key.public_key.x, skey, 32);
-    memcpy(key.public_key.y, skey + 32, 32);
-    memcpy(key.private_key, &pkey, 32);
+    int res = sm2_key_set_private_key(&key, skey);
+    if (res == -1)
+    {
+        return stbox::stx_status::sm2_point_generate_error;
+    }
+
     size_t siglen;
-    sm2_sign(&key, data, sig, &siglen);
+    int sign_res = sm2_sign(&key, hash, sig, &siglen);
+    if (sign_res == -1)
+    {
+        return stbox::stx_status::sm2_sign_error;
+    }
+
     return stbox::stx_status::success;
 }
 
@@ -47,13 +66,28 @@ uint32_t sm2_ecc::verify_signature(const uint8_t *data, uint32_t data_size,
                           const uint8_t *sig, uint32_t sig_size,
                           const uint8_t *public_key,
                           uint32_t pkey_size){
-    uint8_t private_key;
-    sm2_ecc::generate_pkey_from_skey(public_key, pkey_size, &private_key, 64);
+    uint8_t hash[32];
+    sm3_hash::msg_hash(data, data_size, hash, 32);
     SM2_KEY key;
-    memcpy(key.public_key.x, public_key, 32);
-    memcpy(key.public_key.y, public_key + 32, 32);
-    memcpy(key.private_key, &private_key, 32);
-    sm2_verify(&key, data, sig, (size_t)sig_size);
+    SM2_POINT sm2_public_key;
+    int convert_res = sm2_point_from_octets(&sm2_public_key, public_key, 64);
+    if (convert_res == -1)
+    {
+        return stbox::stx_status::sm2_point_convert_error;
+    }
+
+    int res = sm2_key_set_public_key(&key, &sm2_public_key);
+    if (res == -1)
+    {
+        return stbox::stx_status::sm2_empty_public_error;
+    }
+
+    int verify_res = sm2_verify(&key, hash, sig, (size_t)sig_size);
+    if (verify_res == -1)
+    {
+        return stbox::stx_status::sm2_verify_error;
+    }
+
     return stbox::stx_status::success;
 }
 
@@ -61,19 +95,28 @@ uint32_t ecdh_shared_key(const uint8_t *skey, uint32_t skey_size,
                          const uint8_t *public_key, uint32_t pkey_size,
                          uint8_t *shared_key,
                          uint32_t shared_key_size){
-    uint8_t pkey;
-    sm2_ecc::generate_pkey_from_skey(skey, skey_size, &pkey, 64);
     SM2_KEY key;
-    memcpy(key.public_key.x, skey, 32);
-    memcpy(key.public_key.y, skey + 32, 32);
-    memcpy(key.private_key, &pkey, 32);
-    SM2_POINT peer_public;
-    SM2_POINT shared;
-    memcpy(peer_public.x, public_key, 32);
-    memcpy(peer_public.y, public_key + 32, 32);
-    sm2_verify(key, &peer_public, &shared);
-    memcpy(out, shared.x, 32);
-    memcpy(out + 32, shared.y, 32);
+    int res = sm2_key_set_private_key(&key, skey);
+    if (res == -1)
+    {
+        return stbox::stx_status::sm2_point_generate_error;
+    }
+
+    SM2_POINT peer_public, out;
+    int convert_res = sm2_point_from_octets(&peer_public, public_key, 64);
+    if (convert_res == -1)
+    {
+        return stbox::stx_status::sm2_point_convert_error;
+    }
+
+    int ecdh_res = sm2_ecdh(&key, &peer_public, &out);
+    if (ecdh_res == -1)
+    {
+        return stbox::stx_status::sm2_shared_key_error;
+    }
+
+    memcpy(shared_key, out.x, 32);
+    memcpy(shared_key + 32, out.y, 32);
     return stbox::stx_status::success;
 }
 
