0312950e0f5e059e406365bbb386864b9bcd8c80
SishiCheng <70729850+SishiCheng@users.noreply.github.com>
2022-05-31 02:59:34 -0400

    Update sm4_aes.cpp

--- YeeZ-Privacy-Computing/core/src/ypc/crypto/gmssl/sm4_aes.cpp
+++ YeeZ-Privacy-Computing/core/src/ypc/crypto/gmssl/sm4_aes.cpp
@@ -1,4 +1,6 @@
 #include "corecommon/crypto/gmssl/sm4_aes.h"
+//#include "stbox/src/usgx/stx_common_untrusted.cpp"
+#include <glog/logging.h>
 #include <gmssl/sm4.h>
 #include "common/byte.h"
 #include "stbox/stx_status.h"
@@ -20,8 +22,8 @@ namespace crypto{
                                               uint32_t data_size,
                                               uint32_t prefix, uint8_t *cipher,
                                               uint32_t cipher_size){
-    if (key_size != 16) {
-      LOG(ERROR) << "invalid key size: " << key_size << ", expect 16!";
+    if (pkey_size != 16) {
+      LOG(ERROR) << "invalid key size: " << pkey_size << ", expect 16!";
       return stbox::stx_status::ecc_invalid_aes_key_size;
     }
 
@@ -37,12 +39,12 @@ namespace crypto{
     uint8_t mac_text[AAD_MAC_TEXT_LEN];
     memset(mac_text, 0, AAD_MAC_TEXT_LEN);
     memcpy(mac_text, aad_mac_text, AAD_MAC_TEXT_LEN);
-    size_t tag;
+    uint8_t tag;
     sm4_gcm_encrypt(&key, p_iv_text, SM4_GCM_IV_DEFAULT_SIZE, mac_text, AAD_MAC_TEXT_LEN, data, data_size, cipher, 16, &tag);
     return stbox::stx_status::success;       
   }
 
-  uint32_t sm4_aes::get_decrypt_message_size_with_prefix(uint32_t cipher_size);
+  uint32_t sm4_aes::get_decrypt_message_size_with_prefix(uint32_t cipher_size){}
   uint32_t sm4_aes::decrypt_message_with_prefix(const uint8_t *private_key,
                                               uint32_t private_key_size,
                                               const uint8_t *cipher,
