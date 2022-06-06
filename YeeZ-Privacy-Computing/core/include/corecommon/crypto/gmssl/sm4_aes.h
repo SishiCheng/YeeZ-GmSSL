332e53475f914cc0abf95f79cb0ad6e78bd21ccc
SishiCheng <70729850+SishiCheng@users.noreply.github.com>
2022-06-06 15:37:08 -0400

    Update sm4_aes.h

--- YeeZ-Privacy-Computing/core/include/corecommon/crypto/gmssl/sm4_aes.h
+++ YeeZ-Privacy-Computing/core/include/corecommon/crypto/gmssl/sm4_aes.h
@@ -6,13 +6,16 @@ namespace crypto{
 
 class sm4_aes{
 public:
-  static uint32_t get_encrypt_message_size_with_prefix(uint32_t data_size);
+  static inline uint32_t get_mac_code_size() { return 16; }
+  static uint32_t get_cipher_size(uint32_t data_size);
+  static uint32_t get_data_size(uint32_t cipher_size);
+  static inline uint32_t get_key_size() { return 16; }
 
   static uint32_t encrypt_with_prefix(const uint8_t *key, uint32_t key_size,
                                         const uint8_t *data, uint32_t data_size,
                                         uint32_t prefix, uint8_t *cipher,
                                         uint32_t cipher_size, uint8_t *out_mac);
-  static uint32_t get_decrypt_message_size_with_prefix(uint32_t cipher_size);
+
   static uint32_t decrypt_with_prefix(const uint8_t *key, uint32_t key_size,
                                     const uint8_t *cipher, uint32_t cipher_size,
                                     uint32_t prefix, uint8_t *data,
