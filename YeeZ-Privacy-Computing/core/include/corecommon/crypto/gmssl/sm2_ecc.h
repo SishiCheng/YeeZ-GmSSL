5d58f895a696370e383881cb128456e56d281275
SishiCheng <70729850+SishiCheng@users.noreply.github.com>
2022-06-06 15:31:30 -0400

    Update sm2_ecc.h

--- YeeZ-Privacy-Computing/core/include/corecommon/crypto/gmssl/sm2_ecc.h
+++ YeeZ-Privacy-Computing/core/include/corecommon/crypto/gmssl/sm2_ecc.h
@@ -6,12 +6,15 @@ namespace crypto {
 struct sm2_ecc {
   inline static uint32_t get_private_key_size();
   inline static uint32_t get_public_key_size();
+
   static uint32_t gen_private_key(uint32_t skey_size, uint8_t *skey);
 
   static uint32_t generate_pkey_from_skey(const uint8_t *skey,
                                           uint32_t skey_size, uint8_t *pkey,
                                           uint32_t pkey_size);
 
+  static uint32_t get_signature_size();
+  
   static uint32_t sign_message(const uint8_t *skey, uint32_t skey_size,
                                const uint8_t *data, uint32_t data_size,
                                uint8_t *sig, uint32_t sig_size);
