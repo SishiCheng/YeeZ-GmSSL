510559e2576cdcd24327d2c772e021f03a466fa4
SishiCheng <70729850+SishiCheng@users.noreply.github.com>
2022-06-28 00:01:56 -0500

    fix the error the include dir

--- YeeZ-Privacy-Computing/core/include/corecommon/crypto/gmssl.h
+++ YeeZ-Privacy-Computing/core/include/corecommon/crypto/gmssl.h
@@ -1,13 +1,13 @@
 #pragma once
-#include "corecommon/crypto/stdeth/sm2_ecc.h"
-#include "corecommon/crypto/stdeth/sm3_hash.h"
-#include "corecommon/crypto/stdeth/sm4_aes.h"
+#include "corecommon/crypto/gmssl/sm2_ecc.h"
+#include "corecommon/crypto/gmssl/sm3_hash.h"
+#include "corecommon/crypto/gmssl/sm4_aes.h"
 
 #include "corecommon/crypto/crypto_pack.h"
 
 namespace ypc {
 namespace crypto {
 typedef crypto_pack<sm2_ecc, sm3_hash, sm4_aes, sm2_ecc>
-    eth_sgx_crypto;
+    gmssl_sgx_crypto;
 }
 } // namespace ypc
